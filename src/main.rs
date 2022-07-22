f653e09ce16da5b9b156aae9a3baeab5844f2791
bomky <bom_ky@outlook.com>
2022-07-22 10:45:30 +0800

    添加依赖项与测试

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::{fmt, error::Error, collections::HashMap};
+
+use tera::{Context, Tera};
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+     static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera.autoescape_on(vec![".html", ".sql"]);
+        // tera.register_filter("do_nothing", do_nothing_filter);
+        println!("Tera initialized:{:?}",tera);
+        tera
+    };
+}
 fn main() {
-    println!("Hello, world!");
+    let mut context = Context::new();
+    context.insert("username", &"Bob");
+    context.insert("numbers", &vec![1, 2, 3]);
+    context.insert("show_all", &false);
+    context.insert("bio", &"<script>alert('pwnd');</script>");
+
+    // A one off template
+    Tera::one_off("hello", &Context::new(), true).unwrap();
+
+    match TEMPLATES.render("users/profile.html", &context) {
+        Ok(s) => println!("{}", s),
+        Err(e) => {
+            println!("Error: {}", e);
+            let mut cause = e.source();
+            while let Some(e) = cause {
+                println!("Reason: {}", e);
+                cause = e.source();
+            }
+        }
+    };
+
+    // let result = TEMPLATES.render("hello.html", &context).unwrap();
+    // println!("{}", result);
 }
