acbc33aef51fe23baf158c93a0de59e6f6245165
bomky <bom_ky@outlook.com>
2022-09-26 17:31:20 +0800

    添加生成React代码的依赖属性

--- c#/CodeGeneratorApp/CodeGeneratorApp/Modules/CodeGeneratorApp.Modules.ReactGenerate/ViewModels/ViewAViewModel.cs
+++ c#/CodeGeneratorApp/CodeGeneratorApp/Modules/CodeGeneratorApp.Modules.ReactGenerate/ViewModels/ViewAViewModel.cs
@@ -1,21 +1,38 @@
 ﻿using Prism.Mvvm;
+using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using System.Reactive;
 
 namespace CodeGeneratorApp.Modules.ReactGenerate.ViewModels
 {
+
     public class ViewAViewModel : BindableBase
     {
-        private string _message;
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
-        }
         [Reactive]
         public string Title { get; set; } = "Abp React生成";
+
+        #region 生成代码相关
+
+        [Reactive]
+        public string EntityPath { get; set; }
+        [Reactive]
+        public bool CreateApi { get; set; }
+        [Reactive]
+        public bool ApiPrefix { get; set; }
+        [Reactive]
+        public bool CreateStore { get; set; }
+        [Reactive]
+        public bool CreatePage { get; set; }
+        [Reactive]
+        public bool Format { get; set; }
+        public Interaction<Unit, string> SelectEntityInteraction { get; } = new Interaction<Unit, string>();
+        public ReactiveCommand<Unit, Unit> SelectEntity { get; set; }
+        public ReactiveCommand<Unit, Unit> Generate { get; set; }
+
+        #endregion
+
         public ViewAViewModel()
         {
-            Message = "View A from your Prism Module";
         }
     }
 }
