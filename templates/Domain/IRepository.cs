76240cec0a3edb3b64720e50048879a7c5eedb00
bomky <bom_ky@outlook.com>
2022-11-07 10:46:41 +0800

    仓库接口添加默认使用说明接口

--- templates/Domain/IRepository.cs
+++ templates/Domain/IRepository.cs
@@ -1,4 +1,8 @@
+using JetBrains.Annotations;
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Volo.Abp.Domain.Repositories;
 
 namespace {{namespace}}.{{entities}}
@@ -9,6 +13,10 @@ namespace {{namespace}}.{{entities}}
     /// </summary>
     public interface I{{entity}}Repository : IBasicRepository<{{entity}}, {{generic_type}}>
     {
-        
+        //为每个接口添加一个CancellationToken的可选参数,如果只是返回单个对象，添加一个默认值为true的includeDetails参数
+        Task<{{entity}}> FindByName([NotNull] string name, bool includeDetails = true, CancellationToken cancellationToken = default);
+        //如果返回的是一个集合，那么includeDetails参数应该默认为false
+        Task<List<{{entity}}>> GetAll([NotNull] string name, bool includeDetails = false, CancellationToken cancellationToken = default);
+        //除非对性能有很高的要求，一般不用返回映射类（只是返回实体某部分属性）
     }
 }
